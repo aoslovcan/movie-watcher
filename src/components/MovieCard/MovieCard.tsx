126490e925dc227c69bd84ba0f8b4e34952d6f74
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2023-09-01 17:49:45 +0200

    create MovieCard component

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BsFillBookmarkFill } from "react-icons/bs";
+
+type MovieCardProps = {
+  id: string,
+  title: string,
+  posterPath: string,
+}
+
+const MovieCard = ({id, title, posterPath }: MovieCardProps) => {
+
+  const url = `https://image.tmdb.org/t/p/w200/${posterPath}`
+
+  return (
+    <div
+      role="button"
+      tabIndex={0}
+      className="card"
+    >
+      <BsFillBookmarkFill className="favourites-button"/>
+      <img alt={title} src={url}/>
+    </div>
+  );
+}
+
+export default MovieCard;
