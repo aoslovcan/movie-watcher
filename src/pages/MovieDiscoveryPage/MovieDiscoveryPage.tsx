bcd7a921745d92814a2ce97b658fc8385114d7c0
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2023-09-02 17:42:57 +0200

    fix Discovers page layout

--- src/pages/MovieDiscoveryPage/MovieDiscoveryPage.tsx
+++ src/pages/MovieDiscoveryPage/MovieDiscoveryPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import MovieList from "../../components/MovieList/MovieList";
+import { Tile } from "../../components/index";
 
 const MovieDiscoveryPage = () => {
   return (
     <div className="pg-lyt">
-      <div className="discovery">
+     {/* <div className="discovery">
         <div className="discovery__hero">
           <div className="discovery__hero__header">
             <div className="discovery__hero__header__left">
@@ -16,14 +17,22 @@ const MovieDiscoveryPage = () => {
                 </div>
               </div>
             </div>
-            {/*TODO add right icons and modal for choosing streaming services*/}
 
         </div>
-      </div>
-        <div>
+      </div>*/}
+      <Tile title="Discover daily" leftComponent={ <div className="calendar-icon">
+        <div className="month">SEP</div>
+        <div className="date">01</div>
+      </div>}>
+       hello
+      </Tile>
+        <Tile title="Newest movies">
           <MovieList/>
-        </div>
-      </div>
+        </Tile>
+
+        <Tile title="Popular movies">
+          <MovieList/>
+        </Tile>
     </div>
   );
 };
