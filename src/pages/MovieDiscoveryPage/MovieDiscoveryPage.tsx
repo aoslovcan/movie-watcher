e164f5a34ef410f09b608b074308c05d8c5dd2f1
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2023-09-02 18:18:02 +0200

    calendar icon style fix arrow on horizontal scroll

--- src/pages/MovieDiscoveryPage/MovieDiscoveryPage.tsx
+++ src/pages/MovieDiscoveryPage/MovieDiscoveryPage.tsx
@@ -3,27 +3,17 @@ import MovieList from "../../components/MovieList/MovieList";
 import { Tile } from "../../components/index";
 
 const MovieDiscoveryPage = () => {
+
+  const dateInfo = (
+    <div className="calendar-icon">
+      <div className="calendar-icon__month">SEP</div>
+      <div className="calendar-icon__date">1</div>
+    </div>
+  )
+
   return (
     <div className="pg-lyt">
-     {/* <div className="discovery">
-        <div className="discovery__hero">
-          <div className="discovery__hero__header">
-            <div className="discovery__hero__header__left">
-              <div className="discovery__hero__header_title">
-                <h1>Discovery page</h1>
-                <div className="calendar-icon">
-                  <div className="month">SEP</div>
-                  <div className="date">01</div>
-                </div>
-              </div>
-            </div>
-
-        </div>
-      </div>*/}
-      <Tile title="Discover daily" leftComponent={ <div className="calendar-icon">
-        <div className="month">SEP</div>
-        <div className="date">01</div>
-      </div>}>
+      <Tile title="Discover daily" leftComponent={dateInfo}>
        hello
       </Tile>
         <Tile title="Newest movies">
