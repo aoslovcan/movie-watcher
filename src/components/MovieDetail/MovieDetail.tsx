ae4d934348cd1920296a8b23122010a01053cd24
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2023-09-04 15:17:39 +0200

    create movie detail component and style

---
+++ src/components/MovieDetail/MovieDetail.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const MovieDetail = () => {
+return(
+  <div className="movie-detail">
+    <div className="movie-detail__info">
+      <div className="movie-detail__info__item">
+        <h3>Izvorni naziv</h3>
+        <span>
+        7.5
+      </span>
+      </div>
+
+      <div className="movie-detail__info__item">
+        <h3>Ocjena</h3>
+        <span>
+        7.5
+      </span>
+      </div>
+      <div className="movie-detail__info__item">
+        <h3>Žanrovi</h3>
+        <span>
+        7.5
+      </span>
+      </div>
+      <div className="movie-detail__info__item">
+        <h3>Godina proizvodnje</h3>
+        <span>
+        7.5
+      </span>
+      </div>
+
+      <div className="movie-detail__info__item">
+        <h3>Sinopsis</h3>
+        <p className="sinopsis">
+          Dvadeset godina nakon događaja iz filma Zvjezdane staze: Nemesis, pratimo sada umirovljenog admirala Picarda u sljedećem poglavlju njegova života.
+        </p>
+      </div>
+    </div>
+
+  </div>
+)
+}
+
+export default MovieDetail;
--- src/pages/MovieDetailsPage/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MovieCard from "../../components/MovieCard/MovieCard";
 import {Tile} from "../../components/index"
+import MovieDetail from "../../components/MovieDetail/MovieDetail";
 
 const MovieDetailsPage = () => {
 
@@ -8,20 +9,22 @@ const MovieDetailsPage = () => {
     backgroundImage: 'url("https://image.tmdb.org/t/p/w500/4m1Au3YkjqsxF8iwQy0fPYSxE0h.jpg")',
   }
 
+  const title = `Meg 2`
+
   return (
     <div className="movie-details-page">
       <div className="movie-details-page__header" style={innerStyle}/>
       <div className="movie-details-page__box">
-        <div className="right">
+        <div className="left">
           <MovieCard
             title="The movie"
             posterPath="4m1Au3YkjqsxF8iwQy0fPYSxE0h.jpg"
             id={1}
           />
         </div>
-        <div className="left">
-<Tile title="Meg 2">
-  Movie details goes here
+        <div className="right">
+<Tile title={title}>
+ <MovieDetail/>
 </Tile>
         </div>
       </div>
