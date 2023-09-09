25a9096a77c55ec698035641a2156ee41661fde7
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2023-09-09 13:14:47 +0200

    new PosterPicture component

---
+++ src/common/PosterPicture/PosterPicture.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type PosterPictureProps = {
+  width: string;
+  imgPath: string;
+  altTitle: string;
+}
+
+const PosterPicture = ({width = "500", imgPath, altTitle} : PosterPictureProps) => {
+
+const url = `${process.env.REACT_APP_PICTURE_SOURCE}/w${width}${imgPath}`
+
+  return (
+    <picture>
+      <img src={url} alt={altTitle}/>
+    </picture>
+  );
+};
+
+export default PosterPicture;
--- src/common/commonIndex.tsx
+++ src/common/commonIndex.tsx
@@ -1,5 +1,6 @@
 import Button from "./Button/Button";
-import {buttonType, buttonColor, buttonSize}  from "./Button/buttonSettings";
+import { buttonType, buttonColor, buttonSize } from "./Button/buttonSettings";
 import Modal from "./Modal/Modal";
+import PosterPicture from "./PosterPicture/PosterPicture";
 
-export {Button, buttonSize, buttonType, buttonColor, Modal}
+export { Button, buttonSize, buttonType, buttonColor, Modal, PosterPicture };
